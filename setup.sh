02a7f6e218969084909b8e1dcaceec968e91dd9b
Smith Cruise <chendingchao1@126.com>
2023-07-21 22:55:46 +0800

    update

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~
+
+if [[ $(echo $SHELL) != *"/zsh" ]]; then
+  echo "Error: zsh is not the default shell. Please set zsh as your default shell before continuing."
+  exit 1
+fi
+
+# install space theme
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+omz theme set spaceship
+
+
+# install plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+omz plugin enable extract git zsh-autosuggestions zsh-syntax-highlighting
