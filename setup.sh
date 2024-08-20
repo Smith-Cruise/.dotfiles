7763cfb6f05dcc36940713656a5b3d47010695f5
Smith Cruise <chendingchao1@126.com>
2024-08-20 13:34:58 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,14 +10,15 @@ if [[ $(echo $SHELL) != *"/zsh" ]]; then
 fi
 
 # install space theme
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-omz theme set spaceship
+# git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+# ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+# omz theme set spaceship
 
 
 # install plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-omz plugin enable extract git zsh-autosuggestions zsh-syntax-highlighting
+# omz plugin enable extract git zsh-autosuggestions zsh-syntax-highlighting
+omz plugin enable extract zsh-autosuggestions zsh-syntax-highlighting
 
 source ~/.zshrc
