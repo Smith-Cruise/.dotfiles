724d5ed05f63a33771982e8d09f81e183f3a9467
Smith Cruise <chendingchao1@126.com>
2024-08-20 13:03:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 cd ~
 
-sudo apt install zsh tmux vim curl -y
+sudo apt install zsh tmux vim curl software-properties-common -y
 
 # install helix
 sudo add-apt-repository ppa:maveonair/helix-editor
@@ -13,12 +13,12 @@ sudo apt install helix
 # todo
 
 # install tmux
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
+# git clone https://github.com/gpakosz/.tmux.git
+# ln -s -f .tmux/.tmux.conf
+# cp .tmux/.tmux.conf.local .
 
 # install space vim
 # curl -sLf https://spacevim.org/install.sh | bash
 
 # install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
